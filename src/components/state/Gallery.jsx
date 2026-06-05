63a1082cbc3f574cfb60d8250ebd2c576748d483
TongmengHAI <tongmeng016@gmail.com>
2026-06-05 20:15:44 +0700

    useState() with Object

--- src/components/state/Gallery.jsx
+++ src/components/state/Gallery.jsx
@@ -2,20 +2,119 @@ import { useState } from "react";
 import { list } from "./data.js";
 
 export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+  const [page, setPage] = useState(0);
+  const [person, setPerson] = useState({
+    name: "",
+    age: 10,
+    sex: "male",
+    address: {
+      city: "",
+      country: "Cambodia",
+    },
+  });
+
+  function updatePersonName(event) {
+    // console.log(person);
+    // console.log(event.target.value)
+
+    // person.name = event.target.value;
+    // setPerson(person.name = event.target.value);
+
+    // spread operator
+    setPerson({
+      ...person,
+      name: event.target.value,
+    });
+
+    console.log(person);
+  }
+  function updatePersonAge(event) {
+    // spread operator
+    setPerson({
+      ...person,
+      age: event.target.value,
+    });
+
+    console.log(person);
+  }
+
+  // const [person, setPerson] = useState({
+  //   name: "",
+  //   age: 10,
+  //   sex: "male",
+  //   address: {
+  //     city: "",
+  //     country: "Cambodia",
+  //   },
+  // });
+
+  function updatePersonAddr(event) {
+    // spread operator
+    // setPerson({
+    //   ...person,
+    //   age: event.target.value,
+    // });
+
+    setPerson({
+      ...person,
+      address: {
+        ...person.address,
+        city: event.target.value,
+      }
+    });
+
+    console.log(person);
+  }
+
+  // Immer
+
 
   function handleNextClick() {
-    setIndex(index + 1);
+    console.log(list.length);
+
+    // page = 12 list.length = 12 -1 = 11
+    if (page < list.length - 1) {
+      setPage(page + 1);
+    } else {
+      setPage(0);
+    }
+
+    // if (page === list.length - 1) setPage(0);
+
+    // ternary operator
+    // page < list.length - 1 ? setPage(page + 1) : setPage(0);
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = list[index];
+  let sculpture = list[page];
   return (
     <>
+      <div className="mb-4">
+        <label> Name</label>
+        <input type="text" onKeyUp={updatePersonName}></input>
+      </div>
+      <div className="mb-4">
+        <label> Age</label>
+        <input type="text" onKeyUp={updatePersonAge}></input>
+      </div>
+
+      <div className="mb-4">
+        <label> Sex</label>
+        <input
+          type="text"
+          onKeyUp={(event) => {
+            setPerson({
+              ...person,
+              sex: event.target.value,
+            });
+
+          }}
+        ></input>
+      </div>
+      <div className="mb-4">
+        <label> City</label>
+        <input type="text" onKeyUp={updatePersonAddr}></input>
+      </div>
+
       <button
         className=" bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
         onClick={handleNextClick}
@@ -27,17 +126,8 @@ export default function Gallery() {
         by {sculpture.artist}
       </h2>
       <h3 className="text-lg font-semibold mb-4">
-        ({index + 1} of {list.length})
+        ({page + 1} of {list.length})
       </h3>
-      <button
-        className=" bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        onClick={handleMoreClick}
-      >
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      <p className="text-gray-700 mt-4 mb-4">
-        {showMore && <p>{sculpture.description}</p>}
-      </p>
       <img src={sculpture.url} alt={sculpture.alt} />
     </>
   );
